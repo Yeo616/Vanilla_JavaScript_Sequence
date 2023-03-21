c6ce0b41aa765bac8a3bf2ff9347f6bb0e5b63de
unknown <guswls9281@bitsol.kr>
2023-03-21 13:20:24 +0900

    delete btn works

--- input.js
+++ input.js
@@ -7,7 +7,39 @@ const infoInput = document.getElementById("infoInput");
 const changeCircle = document.getElementById("progressInfo");
 const infoOutput = document.getElementById("infoOutput");
 
-const deleteBtn = document.getElementById("buttonDiv")
+const deleteDiv = document.getElementById("buttonDiv");
+const deleteBtn = document.getElementById("deleteBtn");
+deleteBtn.addEventListener("click",deleteData)
+deleteBtn.style.display = "none";
+const inputInfoDiv = document.getElementById("inputInfoDiv");
+
+async function deleteData(){
+    let email = localStorage.getItem("email");
+    console.log("email : ",email);
+
+    // Fetch API를 사용하여 요청 보내기
+   const response = await fetch(`http://127.0.0.1:8000/delete-info?email=${email}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({"email":email}),
+  });
+
+  //응답이 정상적으로 완료되면
+  if (response.ok) {
+    const data = await response.json();
+    infoOutput.innerHTML = JSON.stringify(data.status);
+    console.log("response : ", data.status);
+
+    // 버튼 색 바뀌기
+    changeCircle.style.backgroundColor = "green";
+    changeCircle.innerText = "delete successfully";
+  } else{
+    changeCircle.style.backgroundColor = "red";
+    changeCircle.innerText = "error";
+    infoOutput.innerHTML = "HTTP error" + response.status;
+    console.error("catch error : ", response.status);
+  }
+}
 
 async function saveData(event) {
   // 폼 제출에 의한 페이지 새로고침 방지
@@ -44,7 +76,9 @@ async function saveData(event) {
     //응답이 정상적으로 완료되면
     if (response.ok) {
       //   infoOutput.innerHTML = JSON.stringify(data);
-      infoOutput.innerHTML = JSON.stringify(data.status);
+
+      infoOutput.innerHTML = "new_info : " + infoValue;
+
       console.log("response : ", data.status);
 
       // 버튼 색 바뀌기
